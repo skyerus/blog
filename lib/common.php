10327ae4c4c3d9182b777953301f812115860a4c
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-11 01:07:56 +0100

    added redirectAndExit function

--- lib/common.php
+++ lib/common.php
@@ -50,8 +50,21 @@ function htmlEscape($html)
 function convertSqlDate($sqlDate)
 {
     /* @var $date DateTime */
-    $date = DateTime::createFromFormat('Y-m-d', $sqlDate);
-    return $date->format('d M Y');
+    $date = DateTime::createFromFormat('Y-m-d H:i:s', $sqlDate);
+    return $date->format('d M Y, H:i');
+}
+
+function redirectAndExit($script)
+{
+    // Get the domain-relative URL (e.g. /blog/whatever.php or /whatever.php) and work
+    // out the folder (e.g. /blog/ or /).
+    $relativeUrl = $_SERVER['PHP_SELF'];
+    $urlFolder = substr($relativeUrl, 0, strrpos($relativeUrl, '/') + 1);
+    // Redirect to the full URL (http://myhost/blog/script.php)
+    $host = $_SERVER['HTTP_HOST'];
+    $fullUrl = 'http://' . $host . $urlFolder . $script;
+    header('Location: ' . $fullUrl);
+    exit();
 }
 
 
