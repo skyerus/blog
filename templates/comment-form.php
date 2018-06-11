e3468eed54c094358cb7e4008f6541121cd7632b
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-12 00:29:00 +0100

    added divs for styling

--- templates/comment-form.php
+++ templates/comment-form.php
@@ -5,11 +5,10 @@
  */
 ?>
 
-<?php // We'll use a rule-off for now, to separate page sections ?>
-<hr />
+
 <?php // Report any errors in a bullet-point list ?>
 <?php if ($errors): ?>
-    <div style="border: 1px solid #ff6666; padding: 6px;">
+    <div class="error box comment-margin">
         <ul>
             <?php foreach ($errors as $error): ?>
                 <li><?php echo $error ?></li>
@@ -18,8 +17,8 @@
     </div>
 <?php endif ?>
 <h3>Add your comment</h3>
-<form method="post">
-    <p>
+<form method="post" class="comment-form">
+    <div>
         <label for="comment-name">
             Name:
         </label>
@@ -29,8 +28,8 @@
             name="comment-name"
             value="<?php echo htmlEscape($commentData['name']) ?>"
         />
-    </p>
-    <p>
+    </div>
+    <div>
         <label for="comment-website">
             Website:
         </label>
@@ -40,8 +39,8 @@
             name="comment-website"
             value="<?php echo htmlEscape($commentData['website']) ?>"
         />
-    </p>
-    <p>
+    </div>
+    <div>
         <label for="comment-text">
             Comment:
         </label>
@@ -51,6 +50,9 @@
             rows="8"
             cols="70"
         ><?php echo htmlEscape($commentData['text']) ?></textarea>
-    </p>
-    <input type="submit" value="Submit comment" />
+    </div>
+
+    <div>
+        <input type="submit" value="Submit comment" />
+    </div>
 </form>
