692fc5c82372de2051e0aaea8ccfba5de1bde3db
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-12 00:28:00 +0100

    refactoring and foreign keys

--- lib/common.php
+++ lib/common.php
@@ -33,7 +33,14 @@ function getDsn()
  */
 function getPDO()
 {
-    return new PDO(getDsn());
+    $pdo = new PDO(getDsn());
+    // Foreign key constraints need to be enabled manually in SQLite
+    $result = $pdo->query('PRAGMA foreign_keys = ON');
+    if ($result === false)
+    {
+        throw new Exception('Could not turn on foreign key constraints');
+    }
+    return $pdo;
 }
 
 /**
@@ -88,12 +95,12 @@ function redirectAndExit($script)
 /**
  * Returns the number of comments for the specified post
  *
+ * @param PDO $pdo
  * @param integer $postId
  * @return integer
  */
-function countCommentsForPost($postId)
+function countCommentsForPost(PDO $pdo, $postId)
 {
-    $pdo = getPDO();
     $sql = "
         SELECT
             COUNT(*) c
@@ -112,11 +119,12 @@ function countCommentsForPost($postId)
 /**
  * Returns all the comments for the specified post
  *
+ * @param PDO $pdo
  * @param integer $postId
+ * @return array
  */
-function getCommentsForPost($postId)
+function getCommentsForPost(PDO $pdo,$postId)
 {
-    $pdo = getPDO();
     $sql = "
         SELECT
             id, name, text, created_at, website
